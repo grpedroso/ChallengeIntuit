88df2c2e47c6d42bd46864d07404758414d4a682
Gabriel Pedroso <gabrielpedrosor@gmail.com>
2024-02-28 19:32:30 -0300

    Ordenando endpoints y usando Swagger

---
+++ ChallengeIntuit.API/Endpoints/ClientesExtensions.cs
@@ -0,0 +1,71 @@
+﻿using ChallengeIntuit.Banco;
+using ChallengeIntuit.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeIntuit.API.Endpoints
+{
+    public static class ClientesExtensions
+    {
+        public static void AddEndPointsClientes(this WebApplication app)
+        {
+            app.MapGet("/Clientes", ([FromServices] DAL<Clientes> dal) =>
+            {
+                return Results.Ok(dal.Listar());
+            });
+
+            app.MapGet("/Clientes/{id}", ([FromServices] DAL<Clientes> dal, int id) =>
+            {
+                var cliente = dal.ListarPor(a => a.Id.Equals(id));
+                if (cliente == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(cliente);
+            });
+            app.MapGet("Clientes/BuscarPorNombre/{nombre}", ([FromServices] DAL<Clientes> dal, string nombre) =>
+            {
+                var cliente = dal.ListarPor(a => a.Nombre.Contains(nombre));
+                if (cliente == null)
+                {
+                    Results.NotFound();
+                }
+                return Results.Ok(cliente);
+            });
+
+            app.MapPost("/Clientes", ([FromServices] DAL<Clientes> dal, [FromBody] Clientes clientes) =>
+            {
+                dal.Adicionar(clientes);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/Clientes/{id}", ([FromServices] DAL<Clientes> dal, int id) =>
+            {
+                var artista = dal.RecuperarPor(a => a.Id.Equals(id));
+                if (artista == null)
+                {
+                    Results.NotFound();
+                }
+                dal.Borrar(artista);
+                return Results.NoContent();
+            });
+            app.MapPut("/Clientes", ([FromServices] DAL<Clientes> dal, [FromBody] Clientes clientes) =>
+            {
+                var clienteUpdate = dal.RecuperarPor(a => a.Id == clientes.Id);
+                if (clienteUpdate == null)
+                {
+                    Results.NotFound();
+                }
+                clienteUpdate.Nombre = clientes.Nombre;
+                clienteUpdate.Apellidos = clientes.Apellidos;
+                clienteUpdate.FechaNacimiento = clientes.FechaNacimiento;
+                clienteUpdate.Cuit = clientes.Cuit;
+                clienteUpdate.Domicilio = clientes.Domicilio;
+                clienteUpdate.TelefonoCelular = clientes.TelefonoCelular;
+                clienteUpdate.Email = clientes.Email;
+
+                dal.Actualizar(clienteUpdate);
+
+            });
+        }
+    }
+}
--- ChallengeIntuit.API/Program.cs
+++ ChallengeIntuit.API/Program.cs
@@ -1,5 +1,6 @@
 using ChallengeIntuit.Banco;
 using ChallengeIntuit.Modelos;
+using ChallengeIntuit.API.Endpoints;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
 
@@ -8,68 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ChallengeIntuitContext>();
 builder.Services.AddTransient<DAL<Clientes>>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => 
 options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
-app.MapGet("/Clientes", ([FromServices]DAL<Clientes> dal) =>
-{
-    return Results.Ok(dal.Listar());
-});
+app.AddEndPointsClientes();
 
-app.MapGet("/Clientes/{id}", ([FromServices]DAL < Clientes > dal, int id) =>
-{
-    var cliente = dal.ListarPor(a => a.Id.Equals(id));
-    if(cliente == null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(cliente);
-});
-app.MapGet("Clientes/BuscarPorNombre/{nombre}", ([FromServices] DAL<Clientes> dal, string nombre) =>
-{
-    var cliente = dal.ListarPor(a => a.Nombre.Contains(nombre));
-    if(cliente == null)
-    {
-        Results.NotFound();
-    }
-    return Results.Ok(cliente);
-});
-
-app.MapPost("/Clientes", ([FromServices]DAL < Clientes > dal, [FromBody]Clientes clientes) =>
-{
-    dal.Adicionar(clientes);
-    return Results.Ok();
-});
-
-app.MapDelete("/Clientes/{id}", ([FromServices] DAL<Clientes> dal, int id) =>
-{
-    var artista = dal.RecuperarPor(a =>a.Id.Equals(id));
-    if(artista == null)
-    {
-        Results.NotFound();
-    }
-    dal.Borrar(artista);
-    return Results.NoContent();
-});
-app.MapPut("/Clientes", ([FromServices] DAL<Clientes> dal, [FromBody] Clientes clientes) =>
-{
-    var clienteUpdate = dal.RecuperarPor(a => a.Id == clientes.Id);
-    if(clienteUpdate == null)
-    {
-        Results.NotFound();
-    }
-    clienteUpdate.Nombre = clientes.Nombre;
-    clienteUpdate.Apellidos = clientes.Apellidos;
-    clienteUpdate.FechaNacimiento = clientes.FechaNacimiento;
-    clienteUpdate.Cuit = clientes.Cuit;
-    clienteUpdate.Domicilio = clientes.Domicilio;
-    clienteUpdate.TelefonoCelular = clientes.TelefonoCelular;
-    clienteUpdate.Email = clientes.Email;
-    
-    dal.Actualizar(clienteUpdate);
-
-});
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
